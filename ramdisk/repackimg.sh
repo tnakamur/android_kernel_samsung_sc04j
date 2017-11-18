32008ce76b9f9df41686e4f72e5784b4d3a010ad
tnakamur <taka.nakamura1009@gmail.com>
2017-11-19 01:24:41 +0900

    build: Version 0.1.0

--- ramdisk/repackimg.sh
+++ ramdisk/repackimg.sh
@@ -25,7 +25,6 @@ if [ -z "$(ls split_img/* 2>/dev/null)" -o -z "$(ls ramdisk/* 2>/dev/null)" ]; t
   exit 1;
 fi;
 
-clear;
 echo " ";
 echo "Android Image Kitchen - RepackImg Script";
 echo "by osm0sis @ xda-developers";
